8a4468587c76353261cfcdc70ed0b9b58c93821c
Serhii Ivanskoi <serhii.ivanskoi@alludo.com>
2025-11-18 11:46:41 +0200

    Try arrow function for defiing selectors

--- components/refinebar.ts
+++ components/refinebar.ts
@@ -3,6 +3,9 @@ import { type Locator, type Page, } from '@playwright/test';
 export class RefineBar {
 
     readonly root: Locator;
+    categorySelector = (productCategory : string): string => `//a[@href="#${productCategory}"]`;
+    subcategorySelector = (productCategory: string, productSubcategory: string): string => `//div[@id="${productCategory}"]//a[contains(text(), "${productSubcategory}")]`;
+    brandSelector = (productBrand: string): string => `//div[@class="brands_products"]//a[contains(@href, "${productBrand}")]`;
 
     constructor(readonly page: Page) {
 
@@ -11,12 +14,12 @@ export class RefineBar {
     }
 
     public async clickCategory(productCategory: string, productSubcategory: string): Promise<void> {
-        await this.root.locator(`//a[@href="#${productCategory}"]`).click();
-        await this.root.locator(`//div[@id="${productCategory}"]//a[contains(text(), "${productSubcategory}")]`).click();
+        await this.root.locator(this.categorySelector(productCategory)).click();
+        await this.root.locator(this.subcategorySelector(productCategory, productSubcategory)).click();
     }
 
     public async clickBrand(productBrand: string): Promise<void> {
-        //XPATH this.root.locator(`//div[@class="brands_products"]//a[contains(@href, "${productBrand}")]`).click();
+        //XPATH this.root.locator(brandSelector(productBrand)).click();
         await this.root.getByRole('link', { name: productBrand }).click();
         await this.page.waitForURL(`**/brand_products/${productBrand}`);
     }
